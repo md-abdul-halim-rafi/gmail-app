55e24b8690bd8a6645edd3537d95dc89530c440c
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2024-01-02 23:45:26 +0600

    Sync emails with subject, body, snippet

--- src/routes/email.ts
+++ src/routes/email.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { sendEmail } from "../services/gmail";
+import { sendEmail, getMails } from "../services/gmail";
 import { OAuthTokens } from "../types";
 
 const router = express.Router();
@@ -21,6 +21,24 @@ router.post("/send", async (req, res) => {
     reqBody.to,
     reqBody.subject,
     reqBody.body
+  );
+
+  res.json(response);
+});
+
+router.post("/sync-mails", async (req, res) => {
+  const reqBody = req.body as OAuthTokens;
+
+  if (!reqBody || !reqBody.access_token || !reqBody.refresh_token || !reqBody.expiry_date) {
+    return res.status(400).send("Missing token");
+  }
+
+  const response = await getMails(
+    {
+      access_token: reqBody.access_token,
+      refresh_token: reqBody.refresh_token,
+      expiry_date: reqBody.expiry_date,
+    },
   );
 
   res.json(response);
--- src/services/gmail.ts
+++ src/services/gmail.ts
@@ -1,16 +1,17 @@
 // services/gmail.ts
 
-import { google } from 'googleapis';
+import { google } from "googleapis";
+import base64url from "base64url";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URI } from "../consts";
 import { OAuth2Client } from "google-auth-library";
 import { OAuthTokens } from "../types";
 
-const gmail = google.gmail('v1');
+const gmail = google.gmail("v1");
 
 const getAuth = (tokens: OAuthTokens) => {
   // Assume you have the tokens object from OAuth2 authentication
   const { access_token, refresh_token, expiry_date } = tokens;
-  
+
   // Set up the OAuth2 client with tokens
   const auth = new OAuth2Client({
     clientId: GOOGLE_CLIENT_ID,
@@ -24,7 +25,7 @@ const getAuth = (tokens: OAuthTokens) => {
   });
 
   return auth;
-}
+};
 
 const refreshToken = async (tokens: OAuthTokens) => {
   const auth = getAuth(tokens);
@@ -36,12 +37,24 @@ const refreshToken = async (tokens: OAuthTokens) => {
   // database.set('tokens', refreshedTokens.credentials);
 
   return refreshedTokens.credentials;
-}
+};
 
 const isTokenExpiring = (expiry_date: number) => {
   const expiry = new Date(expiry_date);
   const now = new Date();
   return now > expiry;
+};
+
+function makeRawMessage(to: string, subject: string, body: string): string {
+  const emailLines = [
+    `To: ${to}`,
+    "MIME-Version: 1.0",
+    "Content-type: text/html;charset=iso-8859-1",
+    `Subject: ${subject}`,
+    "",
+    body,
+  ];
+  return Buffer.from(emailLines.join("\r\n")).toString("base64");
 }
 
 export async function sendEmail(tokens: OAuthTokens, to: string, subject: string, body: string): Promise<any> {
@@ -56,7 +69,7 @@ export async function sendEmail(tokens: OAuthTokens, to: string, subject: string
   try {
     const mailResponse = await gmail.users.messages.send({
       auth,
-      userId: 'me',
+      userId: "me",
       requestBody: {
         raw,
       },
@@ -68,14 +81,50 @@ export async function sendEmail(tokens: OAuthTokens, to: string, subject: string
   }
 }
 
-function makeRawMessage(to: string, subject: string, body: string): string {
-  const emailLines = [
-    `To: ${to}`,
-    'MIME-Version: 1.0',
-    'Content-type: text/html;charset=iso-8859-1',
-    `Subject: ${subject}`,
-    '',
-    body,
-  ];
-  return Buffer.from(emailLines.join('\r\n')).toString('base64');
+export async function getMails(tokens: OAuthTokens): Promise<any> {
+  const auth = getAuth(tokens);
+
+  if (isTokenExpiring(tokens.expiry_date)) {
+    await refreshToken(tokens);
+  }
+
+  try {
+    const mailResponse = await gmail.users.messages.list({
+      auth,
+      userId: "me",
+      labelIds: ["INBOX"],
+    });
+    const messages = mailResponse.data.messages;
+
+    if (!messages) {
+      return [];
+    }
+
+    const promises = [];
+
+    for (const message of messages) {
+      promises.push(
+        gmail.users.messages.get({
+          auth,
+          userId: "me",
+          id: message.id as string,
+        })
+      );
+    }
+
+    const messagesResponse = await Promise.all(promises);
+
+    return messagesResponse.map((message) => ({
+      from: message.data.payload?.headers?.find((header) => header.name === "From")?.value,
+      fullMessage:
+        message.data.payload?.parts && message.data.payload?.parts[0].parts
+          ? base64url.decode(message.data.payload?.parts[0].parts[0].body?.data as string)
+          : message.data.snippet,
+      snippet: message.data.snippet,
+      subject: message.data.payload?.headers?.find((header) => header.name === "Subject")?.value,
+    }));
+  } catch (error: any) {
+    console.error(error);
+    return error.response;
+  }
 }
